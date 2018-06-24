37e8d64a20fbdca5c58a36397275ce4f132c5596
Marko Lazic <mlazic@meritsolutions.com>
2018-06-24 19:35:42 +0200

    Added SwipeCardView.Sample.

---
+++ src/SwipeCardView.Sample/SwipeCardView.Sample/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace SwipeCardView.Sample
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		    this.BindingContext = new MainViewModel();
+		}
+	}
+}
---
+++ src/SwipeCardView.Sample/SwipeCardView.Sample/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SwipeCardView.Sample
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        ObservableCollection<string> cardItems;
+
+        public ObservableCollection<string> CardItems
+        {
+            get => cardItems;
+            set
+            {
+                cardItems = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            cardItems = new ObservableCollection<string>();
+            for (var i = 1; i < 100; i++)
+            {
+                cardItems.Add($"Card {i}");
+            }
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void RaisePropertyChanged([CallerMemberName] string name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}

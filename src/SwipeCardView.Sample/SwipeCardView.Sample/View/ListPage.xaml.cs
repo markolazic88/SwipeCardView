3413b671deaf654e11f05f8d6003518dfc7d368b
Marko Lazic <mlazic@meritsolutions.com>
2020-01-24 10:32:46 -0300

    Add ListPage

---
+++ src/SwipeCardView.Sample/SwipeCardView.Sample/View/ListPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SwipeCardView.Sample.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListPage : ContentPage
+    {
+        public ListPage()
+        {
+            InitializeComponent();
+
+            SwipeCardView.ItemsSource = new List<string>()
+            {
+                "Baboon",
+                "Capuchin Monkey",
+                "Blue Monkey"
+            };
+        }
+    }
+}

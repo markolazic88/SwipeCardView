43332c724e825db444f159dfb570857609cc0f55
Marko Lazic <mlazic@meritsolutions.com>
2020-01-29 17:33:13 -0300

    Update Xamarin.Forms version to 4.2.0.848062 in all projects. Update csproj files.

--- src/SwipeCardView.Sample/SwipeCardView.Sample.iOS/Properties/AssemblyInfo.cs
+++ src/SwipeCardView.Sample/SwipeCardView.Sample.iOS/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SwipeCardView.Sample.iOS")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
